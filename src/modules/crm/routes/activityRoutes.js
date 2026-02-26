78738e79a97887537f048a41832b0d61a107a06b
vaibhav30-45 <vshrivastava0730@gmail.com>
2026-02-26 17:10:26 +0530

    change in activity

--- src/modules/crm/routes/activityRoutes.js
+++ src/modules/crm/routes/activityRoutes.js
@@ -3,12 +3,14 @@ const router = express.Router();
 
 const {
   createActivity,
-  getActivities
+  getAllActivities,
+  getMyActivities,
 } = require("../controllers/activityController");
+
 const { protect } = require("../../../middleware/auth.middleware");
 
-
 router.post("/", protect, createActivity);
-router.get("/", protect, getActivities);
+router.get("/", protect, getAllActivities);
+router.get("/me", protect, getMyActivities);
 
 module.exports = router;
