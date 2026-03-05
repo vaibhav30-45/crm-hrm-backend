1eee42343815a4eb95f73d016871752c43585cf2
raj-nandani <riyarajnandani2004@gmail.com>
2026-03-05 15:59:20 +0530

    fixes done

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,9 +1,13 @@
+import React from "react";
+
+
 const Navbar = () => {
+  const name = localStorage.getItem('user') ? JSON.parse(localStorage.getItem('user')).name : 'Guest';
   return (
     <div className="navbar">
       <input type="text" placeholder="Search..." className="search" />
       <div className="profile">
-        <span>Neha Sharma</span>
+        <span>{name}</span>
         <img
           src="https://i.pravatar.cc/40"
           alt="profile"
